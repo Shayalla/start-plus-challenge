3c718e0c2424b17d1f4b4d0099fbbe88fd946e30
Jessie <miss_sc_jessi@hotmail.com>
2022-07-15 18:28:53 -0300

    cadastra novo usuario

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,27 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
+import { createUser } from '../services/users';
 
 function FormRegister() {
+  const [newUser, setNewUser] = useState({});
   const navigate = useNavigate();
+
+  const handleChange = ({ name, value }) => {
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  const register = async () => {
+    await createUser(newUser);
+    navigate('/');
+  };
 
   return (
     <form>
       <label htmlFor="name">
-        <input type="text" id="name" placeholder="Nome Completo"/>
+        <input
+          type="text"
+          id="name"
+          name="fullName"
+          placeholder="Nome Completo"
+          onChange={ ({target}) => handleChange(target) }
+        />
       </label>
       <label htmlFor="username">
-        <input type="text" id="username" placeholder="Usuário"/>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Usuário"
+          onChange={ ({target}) => handleChange(target) }
+        />
       </label>
       <label htmlFor="email">
-        <input type="email" id="email" placeholder="E-mail"/>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="E-mail"
+          onChange={ ({target}) => handleChange(target) }
+        />
       </label>
       <label htmlFor="cellPhone">
-        <input type="tel" id="cellPhone" placeholder="Telefone"/>
+        <input
+          type="tel"
+          id="cellPhone"
+          name="cellPhone"
+          placeholder="Telefone"
+          onChange={ ({target}) => handleChange(target) }
+        />
       </label>
       <label htmlFor="password">
-        <input type="password" id="password" placeholder="Senha"/>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Senha"
+          onChange={ ({target}) => handleChange(target) }
+        />
       </label>
-      <button type="button">Registar</button>
+      <button type="button" onClick={ register }>Registar</button>
       <button type="button" onClick={ () => navigate('/') }>Cancelar</button>
     </form>
   )
