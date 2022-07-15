730ac29a31550b92773e4bf73edf6a1f5d116436
Jessie <miss_sc_jessi@hotmail.com>
2022-07-15 18:29:17 -0300

    faz o login do usuario

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
+import { loginUser } from '../services/users';
 
 function FormLogin() {
+  const [user, setUser] = useState({});
+  const [isHidden, setIsHidden] = useState(true);
   const navigate = useNavigate();
+
+  const handleChange = ({ name, value }) => {
+    setUser({ ...user, [name]: value });
+  };
+
+  const login = async () => {
+    const token = await loginUser(user);
+
+    if (!token) {
+      setIsHidden(false);
+    } else {
+      localStorage.setItem(
+        'user',
+        JSON.stringify(token),
+      );
+      console.log('tudo certo!!');
+    };
+  };
+
   return (
     <form>
       <label htmlFor="login">
-        <input type="text" id="login" placeholder="usuário"/>
+        <input
+          type="text"
+          id="login"
+          placeholder="e-mail"
+          name="email"
+          onChange={ ({target}) => handleChange(target) }
+        />
       </label>
       <label htmlFor="password">
-        <input type="password" id="password" placeholder="senha"/>
+        <input
+          type="password"
+          id="password"
+          placeholder="senha"
+          name="password"
+          onChange={ ({target}) => handleChange(target) }
+        />
       </label>
-      <button type="button">Entrar</button>
+      <button type="button" onClick={ login }>Entrar</button>
       <button type="button" onClick={ () => navigate('/register') }>Registrar-se</button>
+      <div hidden={isHidden}>Usuário ou senha inválido</div>
     </form>
   )
 };
